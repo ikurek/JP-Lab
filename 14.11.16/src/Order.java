34c98293289558d23c9858191fd58306e1538ad9
Igor Kurek <igorkurek96@gmail.com>
2016-11-14 16:13:29 +0100

    Boilerplate

---
+++ 14.11.16/src/Order.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by igor on 14.11.16.
+ */
+public class Order {
+
+    Integer number;
+    ArrayList<String> pizza;
+    String destination;
+
+
+
+}
